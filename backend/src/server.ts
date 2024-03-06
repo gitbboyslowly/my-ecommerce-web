40de8bab5be419e3f3a33b77ece616f16e80939a
boy <tampong8@gmail.com>
2024-03-07 01:15:01 +0700

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import foodRouter from "./routers/food.router";
+import userRouter from './routers/user.router';
+import { dbConnect } from './configs/database.config';
+dbConnect();
+
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:4200"],
+}));
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
